51254c675856473586b73cc80f60827da0d73b4b
TopperDEL <github_com@tparth.de>
2020-06-11 20:03:22 +0200

    feat: Dummy-Service

---
+++ NutzMich.Shared/Services/AngebotService.cs
@@ -0,0 +1,24 @@
+﻿using NutzMich.Contracts.Interfaces;
+using NutzMich.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutzMich.Shared.Services
+{
+    class AngebotService : IAngebotService
+    {
+        public async Task<List<Angebot>> GetAngeboteAsync()
+        {
+            return new List<Angebot>() { new Angebot() { Ueberschrift = "Produkt 1", Beschreibung = "Beschreibung 1" },
+                                         new Angebot() { Ueberschrift = "Produkt 2", Beschreibung = "Beschreibung 2" },
+                                         new Angebot() { Ueberschrift = "Produkt 3", Beschreibung = "Beschreibung 3" }};
+        }
+
+        public async Task<bool> SaveAngebotAsync(Angebot angebot)
+        {
+            return true;
+        }
+    }
+}
