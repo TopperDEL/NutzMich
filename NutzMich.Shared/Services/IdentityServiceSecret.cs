1086a6145815887f0ee95d13e244f6b8a2496b8d
TopperDEL <github_com@tparth.de>
2020-11-17 20:02:36 +0100

    build: Extract Secrets

---
+++ NutzMich.Shared/Services/IdentityServiceSecret.cs
@@ -0,0 +1,14 @@
+﻿using NutzMich.Contracts.Interfaces;
+using NutzMich.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using uplink.NET.Models;
+
+namespace NutzMich.Shared.Services
+{
+    partial class IdentityService : IIdentityService
+    {
+        internal const string MAIN_ACCESS = Droid.Helpers.Secrets.MAIN_ACCESS;
+    }
+}
